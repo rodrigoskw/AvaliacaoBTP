0de870b3412f08c2bb194f7cb15c8e560b35a55f
Rodrigo <rodrigo.shimokawa@tirolez.com.br>
2021-11-29 12:46:35 -0300

    Primeira versao avaliacao BTP

---
+++ Teste1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Teste1
+{
+    class Program
+    {
+        //TODO
+        /* 
+         * Desenvolva um método que receba a string "abbcbbb". O retorno desse método deverá ser "abcb", 
+         * logo o próximo caracter não deverá ser igual ao anterior, o método deve ser capaz de tratar qualquer string.
+        */
+
+        static void Main(string[] args)
+        {
+
+            String avaliacao = "abbcbbb";
+            
+            string processado = RemoverDuplicidades(avaliacao);
+
+         }
+
+        private static string RemoverDuplicidades(string mensagem)
+        {
+
+            List<string> letras = new List<string>();
+            string ultimaPosicao = string.Empty;
+
+            for (int posicao = 0; posicao < mensagem.Length; posicao++)
+            {
+                string analise = mensagem.Substring(posicao, 1);
+                if (analise != ultimaPosicao)
+                {
+                    letras.Add(analise);
+                    ultimaPosicao = analise;
+                }
+            }
+
+            return string.Join("", letras);
+
+        }
+
+    }
+}
---
+++ Teste2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Teste2
+{
+    /*
+      * TODO
+      * Implementar CanBeConnect para identificar se o Friend A pode se Conectar com o Friend C. Para isso, é necessário ter um friend em comum.
+         - Caso sim, o método deverá retornar True
+         - Caso não, o método deverá retornar False
+      */
+    class Friend
+    {
+        public string Email { get; private set; }
+
+        public ICollection<Friend> Friends { get; private set; }
+
+        public Friend(string email)
+        {
+            this.Email = email;
+            this.Friends = new List<Friend>();
+        }
+
+        public void AddFriendship(Friend friend)
+        {
+            this.Friends.Add(friend);
+        }
+
+        public bool CanBeConnect(Friend friendC)
+        {
+            foreach (var amigo in this.Friends)
+            {
+                if (friendC.Friends.Contains(amigo))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Friend a = new Friend("A");
+            Friend b = new Friend("B");
+            Friend c = new Friend("C");
+
+            a.AddFriendship(b);
+            b.AddFriendship(c);
+
+            if (a.CanBeConnect(c))
+            {
+                a.AddFriendship(c);
+            }
+
+        }
+    }
+}
